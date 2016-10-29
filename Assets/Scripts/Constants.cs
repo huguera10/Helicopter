e0785d2112ff954fc3307371caa26417483923ff
huguera10 <a14014@bcc.unifal-mg.edu.br>
2016-10-29 01:04:59 -0200

    "First Commit"
    Incomplete terrain;
    XBox controller tests with a capsule;

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants{
+    
+    public static string LeftJoystickX { get { return "LeftJoystickX"; } }
+
+}
---
+++ Assets/Scripts/XBoxControl.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XBoxControl : MonoBehaviour
+{
+
+    private Vector3 cyclicVector;
+    private Vector3 pedalsVector;
+    private Vector3 collectiveVector;
+    private CharacterController characterController;
+    private float cyclicSpeed = 100;
+    private float pedalsSpeed = 50;
+    private float collectiveSpeed = 50;
+    private float jumpPower = 150;
+    private float gravity = 150;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveCyclic();
+
+        movePedals();
+
+        moveCollective();
+
+        if (characterController.isGrounded)
+        {
+            Debug.Log("Bateu, seu juvena");
+            cyclicVector.y = 0;
+
+            if (Input.GetButtonDown("A"))
+            {
+                cyclicVector.y = jumpPower;
+            }
+        }
+    }
+
+    private void moveCyclic() {
+        cyclicVector.x = Input.GetAxis(Constants.LeftJoystickX) * cyclicSpeed;
+        cyclicVector.z = Input.GetAxis("LeftJoystickY") * cyclicSpeed;
+
+        cyclicVector.y -= gravity * Time.deltaTime;
+
+        characterController.Move(transform.TransformDirection(cyclicVector) * Time.deltaTime);
+    }
+
+    private void movePedals()
+    {
+        pedalsVector.y = (Input.GetAxis("RT") - Input.GetAxis("LT")) * pedalsSpeed;
+
+        transform.Rotate(pedalsVector * Time.deltaTime);
+    }
+
+    private void moveCollective()
+    {
+        collectiveVector.y = Input.GetAxis("DPadY") * collectiveSpeed;
+
+        characterController.Move(transform.TransformDirection(collectiveVector) * Time.deltaTime);
+    }
+}
