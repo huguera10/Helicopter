5c42e83f38127c960a1dc5376dd4f63d3d533a54
Hugo Luiz Camargo Pinto <a14014@bcc.unifal-mg.edu.br>
2017-02-08 15:54:45 -0200

    Finished Drawing the interface

---
+++ Assets/Scripts/HeaalthBar.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HeaalthBar : MonoBehaviour {
+    public float CurrentHealth { get; set; }
+    public float MaxHealth { get; set; }
+
+    public Slider HealthBar;
+    public Text HealthBarCurrentValue;
+
+	// Use this for initialization
+	void Start () {
+        MaxHealth = 100f;
+        CurrentHealth = MaxHealth;
+
+        HealthBar.value = CalculateHealth();
+        HealthBarCurrentValue.text = CurrentHealth.ToString();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.X))
+            DealDamage(10);
+	}
+
+    void DealDamage(float damageValue)
+    {
+        CurrentHealth -= damageValue;
+        HealthBar.value = CalculateHealth();
+        HealthBarCurrentValue.text = CurrentHealth.ToString();
+
+        if (CurrentHealth <= 0)
+            Die();
+    }
+
+    float CalculateHealth()
+    {
+        return CurrentHealth / MaxHealth;
+    }
+
+    void Die()
+    {
+        CurrentHealth = 0;
+        Debug.Log("Wasted");
+    }
+}
